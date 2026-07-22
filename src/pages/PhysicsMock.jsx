553ca56475cb82a19b439b634fe1b65b903c9e21
Ayush7web <ayushk93516@gmail.com>
2026-07-22 17:52:29 +0530

    fetch data successful

--- src/pages/PhysicsMock.jsx
+++ src/pages/PhysicsMock.jsx
@@ -1,63 +1,26 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const PhysicsMock = () => {
   const [retrieveData, setRetrieveData] = useState([]);
-  const [loading , setLoading] = useState(true);
 
   useEffect(() => {
-    const body = { topic: "physics class 10th" };
-    console.log("sending payload to server:", body);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/questions?topic=physics class 10th",
+        );
+        console.log("Mission Success: API is working good!", response.data.data);
+        setRetrieveData(response.data.data);
+      } catch (error) {
+        console.log("mission failed", error.message);
+      }
+    };
 
-    fetch("http://localhost:5000/api/chat", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(body),
-    })
-    .then((res) =>{
-      if(!res.ok) throw new Error("Server responded with an error");
-    })
-    .then((data) =>{
-      console.log("Data received from node js:" , data);
-      setRetrieveData(data);
-      setLoading(false);
-    })
-    .catch((err) =>{
-      console.error("Fetch operation failure:", err);
-      setLoading(false);
-    });
+    fetchData();
   }, []);
 
-  if(loading) return <div>Generating Physics questions...</div>
-
-
-  return (
-    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
-      <h2>Physics Mock Test (Class 10th)</h2>
-
-      {/* 2. State ke data ko screen par render karna */}
-      {retrieveData.length === 0 ? (
-        <p>No questions found or failed to load.</p>
-      ) : (
-        retrieveData.map((q, index) => (
-          <div
-            key={q.id || index}
-            style={{ margin: "15px 0", borderBottom: "1px solid #ccc" }}
-          >
-            <h4>
-              Q{index + 1}: {q.question}
-            </h4>
-            {q.options?.map((option, optIdx) => (
-              <p key={optIdx}>
-                <input type="radio" name={`question-${index}`} /> {option}
-              </p>
-            ))}
-          </div>
-        ))
-      )}
-    </div>
-  );
+  return <div>Mission code Active</div>;
 };
 
 export default PhysicsMock;
