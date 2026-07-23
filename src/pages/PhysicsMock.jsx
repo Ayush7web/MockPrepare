426074e25ef00f982d8e174f2f226c6d8f6f590d
Ayush7web <ayushk93516@gmail.com>
2026-07-23 18:48:51 +0530

    ..

--- src/pages/PhysicsMock.jsx
+++ src/pages/PhysicsMock.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 
 const PhysicsMock = () => {
   const [retrieveData, setRetrieveData] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState("");
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -10,7 +12,11 @@ const PhysicsMock = () => {
         const response = await axios.get(
           "http://localhost:5000/api/questions?topic=physics class 10th",
         );
-        console.log("Mission Success: API is working good!", response.data.data);
+        // console.log(
+        //   "Mission Success: API is working good!",
+
+        //   response.data.data,
+        // );
         setRetrieveData(response.data.data);
       } catch (error) {
         console.log("mission failed", error.message);
@@ -20,7 +26,22 @@ const PhysicsMock = () => {
     fetchData();
   }, []);
 
-  return <div>Mission code Active</div>;
+  return (
+    <div className="relative top-10 ">
+      <div className="text-3xl text-center font-semibold text-blue-500 shadow-2xl hover:text-blue-700">
+        Topic: Physics <span>For Class 10th</span>
+      </div>
+      {retrieveData.map((item, index) => (
+        <div key={index} className="space-y-2.5 border">
+          <p>{item.topic}</p>
+          <p>{item.questionsText}</p>
+          <p>{item.options}</p>
+          <p>{item.correctAnswer}</p>
+          <p>{item.explanation}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PhysicsMock;
